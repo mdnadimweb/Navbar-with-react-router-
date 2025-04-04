d4621de724da87f659152afb48c6353861a38e22
mdnadimweb <mdnadim442244@gmail.com>
2025-04-04 16:41:18 +0600

    add navigate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,32 @@
+
+import React from "react";
+import { useNavigate } from "react-router";
+
+
 function About() {
+
+        const navigate = useNavigate (); 
+
     return (  <>
     
     <h1>about page </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cum quia optio exercitationem animi tenetur a laborum magnam eligendi omnis, blanditiis error, quibusdam sequi natus similique repudiandae debitis cumque necessitatibus! Nulla molestiae impedit accusamus, maxime voluptatum facere repellendus temporibus explicabo illo dolorem, unde autem incidunt enim itaque, ab officia sequi.</p>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, soluta. Tempora alias et similique voluptates assumenda id omnis doloremque quos consequuntur cumque! Assumenda corporis eius deleniti! Laboriosam odit quia ad et praesentium? Voluptas voluptatibus laboriosam, repudiandae repellendus maxime, omnis, beatae ex nisi magni doloremque ducimus! Fuga pariatur voluptates quisquam tenetur!</p>
+
+<button onClick={()=>{ 
+
+            navigate("/");
+    
+    }}
+    
+    
+    >go to home </button>
+
+
+
+
+
     </>);
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,9 @@
+import { useNavigate } from "react-router";
+
 function Contact() {
+
+    const navigate = useNavigate () ; 
+
     return ( <>
     
     <h1>Contact page </h1>
@@ -6,6 +11,10 @@ function Contact() {
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cum quia optio exercitationem animi tenetur a laborum magnam eligendi omnis, blanditiis error, quibusdam sequi natus similique repudiandae debitis cumque necessitatibus! Nulla molestiae impedit accusamus, maxime voluptatum facere repellendus temporibus explicabo illo dolorem, unde autem incidunt enim itaque, ab officia sequi.</p>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, soluta. Tempora alias et similique voluptates assumenda id omnis doloremque quos consequuntur cumque! Assumenda corporis eius deleniti! Laboriosam odit quia ad et praesentium? Voluptas voluptatibus laboriosam, repudiandae repellendus maxime, omnis, beatae ex nisi magni doloremque ducimus! Fuga pariatur voluptates quisquam tenetur!</p>
+
+<button onClick={()=>{
+    navigate("/"); 
+}}>go to home </button>
     
     
     </> );
