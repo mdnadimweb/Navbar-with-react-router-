32f628c4f2a5a810d34369214a9741ae73e078cd
mdnadimweb <mdnadim442244@gmail.com>
2025-04-04 17:33:12 +0600

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
-import {NavLink} from "react-router";
+import { NavLink } from "react-router";
 import React from "react";
+import logo from "../assets/logo.png"
 
-function Navbar () {
-    return ( <>
-    
-    <nav>
-        <ul>
-
-            <NavLink to= "/">Home</NavLink>
-            <NavLink to= "/about">About</NavLink>
-            <NavLink to= "/contact">Contact</NavLink>
-            <NavLink to= "/login">Login</NavLink>
-
-        </ul>
-    </nav>
-    
-    
-    
-    </> );
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="logo">
+            <img src= {logo} alt="logo for nav" />
+        </div>
+        <div className="links">
+          <ul>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar ;
+export default Navbar;
